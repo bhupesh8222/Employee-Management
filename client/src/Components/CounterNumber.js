65a45690edfda2a87027a3d24a63dd60af1c992b
Bhupesh Bhatt <bhupeshbhatt8222@gmail.com>
2022-07-18 08:44:39 +0530

    final comment

--- client/src/Components/CounterNumber.js
+++ client/src/Components/CounterNumber.js
@@ -6,29 +6,7 @@ import { counterContext } from './Home';
 function CounterNumber() {
 	const counter = useContext(counterContext);
 
-	// const actionA = {
-	// 	type: 'Add',
-	// 	number: 10,
-	// };
-
-	// const initialState = {
-	// 	count: 0,
-	// 	meta: 'TEMP_HPO',
-	// };
-
-	// const reducer = (state, action) => {
-	// 	// let curr_count = curr_state.count;
-
-	// 	const newState = {
-	// 		...state,
-	// 		count: state.count + action.number,
-	// 	};
-	// 	console.log(state);
-	// 	return newState;
-	// };
-
-	// const [state, dispatch] = useReducer(reducer, initialState);
-
+	//MAKING UPDATE TO THE GLOBAL STATE
 	const changeCount = () => {
 		console.log('CLICKED');
 		counter.dispatch();
